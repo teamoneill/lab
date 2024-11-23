ec5989a40ffa425a0cfe00efdada9e36abb80a25
Michael O'Neill <38654215+teamoneill@users.noreply.github.com>
2024-11-23 14:11:37 -0600

    Update whoami.sql
    Added header, pretty changes to output

--- oracle/sqlpath/whoami.sql
+++ oracle/sqlpath/whoami.sql
@@ -1,3 +1,34 @@
+/*
+   whoami.sql
+   
+   Example:
+      Copy into a client's SQLPATH location
+      Execute without pathing
+      sql> @whoami
+
+   MIT License
+
+   Copyright (c) 2024 Team O'Neill Projects and Michael O'Neill
+   
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
 set termout off
 
 column service_name new_value service_name
@@ -8,7 +39,6 @@ column container_name new_value container_name
 column cdb_name new_value cdb_name
 column session_edition new_value session_edition
 column database_default_edition new_value database_default_edition
-
 
 select sys_context('USERENV','SERVICE_NAME') as service_name
    , sys_context('USERENV','SESSION_USER') || decode(sys_context('USERENV','IP_ADDRESS'),'TRUE',' (ISDBA)',null) as session_user
@@ -22,11 +52,13 @@ where property_name = 'DEFAULT_EDITION';
 
 set termout on
 
-prompt SERVICE NAME...... &service_name
-prompt SESSION USER...... &session_user
-prompt CURRENT SCHEMA.... &current_schema
-prompt CONTAINER NAME.... &container_name
-prompt CDB............... &cdb_name
-prompt SESSION EDITION... &session_edition
-prompt DATABASE DEFAULT.. &database_default_edition
+prompt
+prompt Oracle session whoami
+prompt SERVICE NAME............... &service_name
+prompt SESSION USER............... &session_user
+prompt CURRENT SCHEMA............. &current_schema
+prompt CONTAINER NAME............. &container_name
+prompt CDB NAME................... &cdb_name
+prompt SESSION EDITION............ &session_edition
+prompt DATABASE DEFAULT EDITION... &database_default_edition
 prompt
