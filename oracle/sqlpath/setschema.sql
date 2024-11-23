5ff27eed44c2b3f7588d2eaa108e78485e9807ed
Michael O'Neill <38654215+teamoneill@users.noreply.github.com>
2024-11-23 14:30:42 -0600

    Create setschema.sql
    alter session helper

---
+++ oracle/sqlpath/setschema.sql
@@ -0,0 +1,42 @@
+/*
+   setschema.sql
+   
+   Spares user some typing and automatically provides feedback.
+   
+   Requires whoami.sql
+   
+   Example:
+      Copy into a client's SQLPATH location
+      Execute without pathing
+      sql> @setschema
+
+   MIT License
+
+   Copyright (c) 2024 Team O'Neill Projects and Michael O'Neill
+   
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
+set termout off
+
+alter session set current_schema = &1 ;
+
+set termout on
+
+@whoami
